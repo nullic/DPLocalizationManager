93935406c147663bb3ee03c1fbe465cbc21a735b
Dmitriy Petrusevich <nullic@gmail.com>
2019-09-26 17:11:07 +0300

    Package: .dynamic type

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "DPLocalization",
+            type: .dynamic,
             targets: ["DPLocalization"]
         )
     ],
